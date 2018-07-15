b0c3538f5adff0c17da427028c58a035fc6c17fe
Mann <Harleen.Mann@originenergy.com.au>
2018-07-15 18:24:45 +1000

    readthedocs correction for

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..\\'))
+sys.path.insert(0, os.path.abspath('..'))
+print(sys.path.insert)
 #sys.path.insert(0, os.path.abspath('..\\src\\'))
 
 source_parsers = {
